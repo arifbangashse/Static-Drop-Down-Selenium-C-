f20760e1f13ce6cdbd7f61c1762cdc6450186ac2
Arif Bangash <arif.swtester@gmail.com>
2022-11-13 16:21:13 +0100

    E2ETest and AlertAutoSuggestive

---
+++ AlertActionsAutoSuggestive.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class AlertActionsAutoSuggestive
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/AutomationPractice/";
+        }
+
+        [Test]
+        public void Test_Alert()
+        {
+            String name = "Arif";
+            driver.FindElement(By.XPath("//fieldset/input[@id='name']")).SendKeys("Arif");
+            driver.FindElement(By.CssSelector("#confirmbtn")).Click();
+            String alertText = driver.SwitchTo().Alert().Text;
+            driver.SwitchTo().Alert().Accept();
+
+            StringAssert.Contains(name, alertText);
+
+        }
+
+        [Test]
+        public void Test_AutoSuggestiveDropDown()
+        {
+            driver.FindElement(By.Id("autocomplete")).SendKeys("Pa");
+            Thread.Sleep(3000);
+
+            //ArrayList countryList = new ArrayList();
+
+            IList<IWebElement> countryList = driver.FindElements(By.CssSelector(".ui-menu-item div"));
+
+            foreach(IWebElement country in countryList)
+            {
+                if (country.Text.Equals("Pakistan"))
+                {
+                    country.Click();
+                }
+            }
+
+            TestContext.Progress.WriteLine(driver.FindElement(By.Id("autocomplete")).GetAttribute("value"));
+
+            
+        }
+
+    }
+}
+
---
+++ SortingWebTables.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumLearning
+{
+    public class SortingWebTables
+    {
+        IWebDriver driver;
+
+        [SetUp]
+
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/seleniumPractise/#/offers";
+        }
+
+        [Test]
+
+        public void SortingTables()
+        {
+            ArrayList a = new ArrayList();
+            ArrayList b = new ArrayList();
+            SelectElement dropDown = new SelectElement(driver.FindElement(By.Id("page-menu")));
+            dropDown.SelectByValue("20");
+
+            // Store in Arraylist A
+
+            ArrayList arrayListA = new ArrayList();
+
+            IList<IWebElement> productsA = driver.FindElements(By.XPath("//tr/td[1]"));
+
+            foreach(IWebElement product in productsA)
+            {
+                a.Add(product.Text);
+            }
+
+            a.Sort();
+
+            // Click column
+
+            driver.FindElement(By.XPath("//thead/tr/th[1]")).Click();
+
+            // Grab sorted list in Fresh Arraylist B
+            IList<IWebElement> productsB = driver.FindElements(By.XPath("//tr/td[1]"));
+            foreach (IWebElement product in productsB)
+            {
+                b.Add(product.Text);
+            }
+
+            // Arralist A to B = equal
+            CollectionAssert.AreEqual(a, b);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            //driver.Quit();
+        }
+    }
+}
+
